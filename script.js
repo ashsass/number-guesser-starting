b8d993b9d2363edf82d51522111b0947a4022e8b
ashsass <93344091+ashsass@users.noreply.github.com>
2022-07-22 18:47:09 -0400

    created the function compareGuesses

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ let currentRoundNumber = 1;
 const generateTarget = () =>{
     return Math.floor((Math.random() * 9) + 1);
 }
+
+
+//Determines which player wins based on who is the closest to the target number.
+//If there is a tie the human player wins
+//True = human wins
+//False = computer wins
+const compareGuesses = (userGuess, compGuess, targetNum) =>{
+    targetNum = generateTarget();
+    let humanDifference = abs(targetNum - userGuess);
+    let computerDifference abs(targetNum - compGuess); //Use these two variables to determine how far off the guess is from the target and the smaller difference wins
+
+    if(humanDifference === computerDifference){
+        return true;
+    }
+    else if(humanDifference > computerDifference){
+        return false;
+    }
+    return true; 
+}
